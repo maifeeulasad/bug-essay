f5bd721146a78dc91ab2917f33ff198f9cc6846e
Maifee Ul Asad <maifeeulasad@gmail.com>
2026-07-11 16:00:10 +0600

    antlr re-generated `src/grammar/python/PythonTracebackParserListener.ts` for python

--- src/grammar/python/PythonTracebackParserListener.ts
+++ src/grammar/python/PythonTracebackParserListener.ts
@@ -3,9 +3,16 @@
 import {ParseTreeListener} from "antlr4";
 
 
-import { TracebackContext } from "./PythonTracebackParser.js";
+import { LogContext } from "./PythonTracebackParser.js";
+import { EntryContext } from "./PythonTracebackParser.js";
+import { OutputLineContext } from "./PythonTracebackParser.js";
+import { TracebackChainContext } from "./PythonTracebackParser.js";
+import { TransitionContext } from "./PythonTracebackParser.js";
+import { TracebackBlockContext } from "./PythonTracebackParser.js";
 import { TracebackHeaderContext } from "./PythonTracebackParser.js";
 import { FrameContext } from "./PythonTracebackParser.js";
+import { FunctionNameContext } from "./PythonTracebackParser.js";
+import { QualifiedNameContext } from "./PythonTracebackParser.js";
 import { SourceLineContext } from "./PythonTracebackParser.js";
 import { ExceptionLineContext } from "./PythonTracebackParser.js";
 
@@ -16,15 +23,65 @@ import { ExceptionLineContext } from "./PythonTracebackParser.js";
  */
 export default class PythonTracebackParserListener extends ParseTreeListener {
 	/**
-	 * Enter a parse tree produced by `PythonTracebackParser.traceback`.
+	 * Enter a parse tree produced by `PythonTracebackParser.log`.
 	 * @param ctx the parse tree
 	 */
-	enterTraceback?: (ctx: TracebackContext) => void;
+	enterLog?: (ctx: LogContext) => void;
 	/**
-	 * Exit a parse tree produced by `PythonTracebackParser.traceback`.
+	 * Exit a parse tree produced by `PythonTracebackParser.log`.
 	 * @param ctx the parse tree
 	 */
-	exitTraceback?: (ctx: TracebackContext) => void;
+	exitLog?: (ctx: LogContext) => void;
+	/**
+	 * Enter a parse tree produced by `PythonTracebackParser.entry`.
+	 * @param ctx the parse tree
+	 */
+	enterEntry?: (ctx: EntryContext) => void;
+	/**
+	 * Exit a parse tree produced by `PythonTracebackParser.entry`.
+	 * @param ctx the parse tree
+	 */
+	exitEntry?: (ctx: EntryContext) => void;
+	/**
+	 * Enter a parse tree produced by `PythonTracebackParser.outputLine`.
+	 * @param ctx the parse tree
+	 */
+	enterOutputLine?: (ctx: OutputLineContext) => void;
+	/**
+	 * Exit a parse tree produced by `PythonTracebackParser.outputLine`.
+	 * @param ctx the parse tree
+	 */
+	exitOutputLine?: (ctx: OutputLineContext) => void;
+	/**
+	 * Enter a parse tree produced by `PythonTracebackParser.tracebackChain`.
+	 * @param ctx the parse tree
+	 */
+	enterTracebackChain?: (ctx: TracebackChainContext) => void;
+	/**
+	 * Exit a parse tree produced by `PythonTracebackParser.tracebackChain`.
+	 * @param ctx the parse tree
+	 */
+	exitTracebackChain?: (ctx: TracebackChainContext) => void;
+	/**
+	 * Enter a parse tree produced by `PythonTracebackParser.transition`.
+	 * @param ctx the parse tree
+	 */
+	enterTransition?: (ctx: TransitionContext) => void;
+	/**
+	 * Exit a parse tree produced by `PythonTracebackParser.transition`.
+	 * @param ctx the parse tree
+	 */
+	exitTransition?: (ctx: TransitionContext) => void;
+	/**
+	 * Enter a parse tree produced by `PythonTracebackParser.tracebackBlock`.
+	 * @param ctx the parse tree
+	 */
+	enterTracebackBlock?: (ctx: TracebackBlockContext) => void;
+	/**
+	 * Exit a parse tree produced by `PythonTracebackParser.tracebackBlock`.
+	 * @param ctx the parse tree
+	 */
+	exitTracebackBlock?: (ctx: TracebackBlockContext) => void;
 	/**
 	 * Enter a parse tree produced by `PythonTracebackParser.tracebackHeader`.
 	 * @param ctx the parse tree
@@ -45,6 +102,26 @@ export default class PythonTracebackParserListener extends ParseTreeListener {
 	 * @param ctx the parse tree
 	 */
 	exitFrame?: (ctx: FrameContext) => void;
+	/**
+	 * Enter a parse tree produced by `PythonTracebackParser.functionName`.
+	 * @param ctx the parse tree
+	 */
+	enterFunctionName?: (ctx: FunctionNameContext) => void;
+	/**
+	 * Exit a parse tree produced by `PythonTracebackParser.functionName`.
+	 * @param ctx the parse tree
+	 */
+	exitFunctionName?: (ctx: FunctionNameContext) => void;
+	/**
+	 * Enter a parse tree produced by `PythonTracebackParser.qualifiedName`.
+	 * @param ctx the parse tree
+	 */
+	enterQualifiedName?: (ctx: QualifiedNameContext) => void;
+	/**
+	 * Exit a parse tree produced by `PythonTracebackParser.qualifiedName`.
+	 * @param ctx the parse tree
+	 */
+	exitQualifiedName?: (ctx: QualifiedNameContext) => void;
 	/**
 	 * Enter a parse tree produced by `PythonTracebackParser.sourceLine`.
 	 * @param ctx the parse tree
